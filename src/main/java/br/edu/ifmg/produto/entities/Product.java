41d87822f930bc1b6332b6b495fcaea39a290054
WILLY GONCALVES CAMPOS <0026645@ifmg>
2025-04-15 13:53:04 -0300

    aula 7

--- src/main/java/br/edu/ifmg/produto/entities/Product.java
+++ src/main/java/br/edu/ifmg/produto/entities/Product.java
@@ -13,6 +13,7 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
+    @Column(columnDefinition = "TEXT")
     private String description;
     private double price;
     private String imageUrl;
